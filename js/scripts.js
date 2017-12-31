92b8ef3bca26e46ddecdfa9fe729ba6c6be1ac6d
Paweł Galeja <pgaleja@gmail.com>
2017-12-31 17:19:05 +0100

    Create new function in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,8 @@
-/*scripts.js
-Conditional instructions in practice*/
-
-var a = 3;
-var b = 6;
-var value = (a * a) - (2 * a * b) + (b * b);
-console.log("Value= " + value);
-
-if (value > 0) {
-    console.log("Wynik dodatni")
-}
-else if (value < 0) {
-    console.log("Wynik ujemny")
-}
-else {
-    console.log("Wynik równy 0")
-}
+var allNames=[];
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = allNames.concat(femaleNames,maleNames);
+var newName = 'Marian';
+if (allNames.indexOf(newName) === -1) {
+	var namesWithoutMarian = allNames.push('Marian')
+};
